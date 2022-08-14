841b5c917a9781e625422a6581adced7778f9eca
bigsbug <booxgitboox@gmail.com>
2022-08-14 18:27:07 -0400

    create new exception for auth_system

--- account_manager/auth_system/authentication_exceptions.py
+++ account_manager/auth_system/authentication_exceptions.py
@@ -6,7 +6,13 @@ class TokenIsInvalid(Exception):
 
 class TokenExpired(Exception):
     "Token is Expired"
+    ...
+
+
+class OperatoinFailed(Exception):
+    "The Operation Failed"
 
 
 class AuthenticationFailed(Exception):
     "Authentication Failed"
+    ...
