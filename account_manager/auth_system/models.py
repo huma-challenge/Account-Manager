61722b4a1a9d68323f5e39b0a267f5fc802db002
bigsbug <booxgitboox@gmail.com>
2022-08-14 15:28:56 -0400

    fix typo in the BlackListToken model

--- account_manager/auth_system/models.py
+++ account_manager/auth_system/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Using for saving logouted tokens and etc ...
 class BlackListToken(models.Model):
-    token = models.CharField(204, unique=True, blank=False, null=False)
+    token = models.CharField(max_length=204, unique=True, blank=False, null=False)
