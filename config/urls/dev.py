b9d4a6ef93c81bce48e8e73f5659ed4ee3ca63f5
bigsbug <booxgitboox@gmail.com>
2022-08-14 09:21:41 -0400

    Active Services on the Server

--- config/urls/dev.py
+++ config/urls/dev.py
@@ -1,1 +1,13 @@
+import imp
 from config.urls.base import *
+
+from account_manager.api.views import UserService
+from account_manager import account_pb2_grpc
+
+# Active all servicers on the server [channel]
+def grpc_handlers(server):
+
+    # Active UserService as a servicer for UserManager service
+    account_pb2_grpc.add_UserManagerServicer_to_server(
+        UserService.as_servicer(), server
+    )
