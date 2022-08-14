f7e1a0995e637c30d7b5ebea0753c11026942422
bigsbug <booxgitboox@gmail.com>
2022-08-14 18:26:03 -0400

    add some tools for auth_system app

---
+++ account_manager/auth_system/tools.py
@@ -0,0 +1,31 @@
+from account_manager.account_pb2 import UserToken
+
+
+def request_have(request, attr_name: str):
+    """check a request have a specific attribute
+
+    Args:
+        request (Any): A Request obj
+        attr_name (str): attribute name
+
+    Returns:
+        bool : status check
+    """
+
+    try:
+        getattr(request, str(attr_name))
+        return True
+    except AttributeError:
+        return False
+
+
+def request_have_token(request):
+    """Check Request have UserToken
+
+    Args:
+        request (Any): Reqeust obj
+
+    Returns:
+        bool : status of check
+    """
+    return isinstance(request_have(request, "token"), UserToken)
