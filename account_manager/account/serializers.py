917b570e452877c5753cda8f1c2fe5aaf45c8594
bigsbug <booxgitboox@gmail.com>
2022-08-14 09:21:03 -0400

    create serializer and servicer of UserManager service

---
+++ account_manager/account/serializers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+
+from django_grpc_framework import proto_serializers
+from account_manager import account_pb2
+
+# Get active user model
+User = get_user_model()
+
+
+class UserProtoSerializer(proto_serializers.ModelProtoSerializer):
+    class Meta:
+        model = User
+        proto_class = account_pb2.User
+        fields = [
+            "id",
+            "username",
+            "password",
+            "first_name",
+            "last_name",
+            "email",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+        ]
--- account_manager/api/views.py
+++ account_manager/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from django_grpc_framework import generics
+
+from account_manager.account.serializers import UserProtoSerializer
+
+
+# Get active user model
+User = get_user_model()
+
+
+class UserService(generics.ModelService):
+    queryset = User.objects.all()
+    serializer_class = UserProtoSerializer
+
+    def List(self, request, context):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+
+        # This RPC is a Stream RPC so we shold pass data as yield insted of returne
+        for message in serializer.message:
+            yield message
+
+    def Retrieve(self, request, context):
+        print(request)
+        return super().Retrieve(request, context)
