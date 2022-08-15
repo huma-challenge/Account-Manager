37401695d9709f739bd1d8006dcda187694d65b4
bigsbug <booxgitboox@gmail.com>
2022-08-15 18:55:54 -0400

    dockerize project

---
+++ entrypoint.dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Clone proto files from repo ... "
+rm -rf ./protobufs
+git clone https://github.com/huma-challenge/protobufs.git
+
+echo "Compile proto file to python ... "
+python -m grpc_tools.protoc -I./ --python_out=./ --grpc_python_out=./ ./protobufs/account_manager/account.proto
+
+echo "Makemigrations and apply migrates ... "
+python3 manage.py makemigrations --noinput 
+python3 manage.py migrate --noinput 
+
+
+echo "Run gRPC development server ... "
+python ./manage.py grpcrunserver --dev 
+
+
